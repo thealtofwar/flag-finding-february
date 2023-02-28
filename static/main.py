a8aee3eb0a3980424a29cc66ff7083389378f129
thealtofwar <88910093+thealtofwar@users.noreply.github.com>
2023-02-28 09:47:12 -0800

    day 10

---
+++ static/main.py
@@ -0,0 +1,6 @@
+data = input("What is the password? ")
+
+if data[0:5] == "infoS" and data[5:10] == "ecClu" and data[10:15] == "b{pyt" and data[15:20] == "hon_t" and data[20:25] == "rolli" and data[25:30] == "ng_54" and data[30:] == "8dsi}":
+    print("Correct")
+else:
+    print("Incorrect")
